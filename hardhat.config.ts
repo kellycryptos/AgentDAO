8d5237018707d330cd8ca31b350a0005426b701d
kellycryptos <kellycryptos@users.noreply.github.com>
2026-08-06 13:26:14 +0100

    fix: resolve npm peer dependencies and build errors for Vercel deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
-import "@nomicfoundation/hardhat-ignition-viem";
 import * as dotenv from "dotenv";
 
 dotenv.config();
@@ -20,22 +19,8 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {},
     giwaSepolia: {
-      type: "http",
-      chainType: "op",
       url: "https://sepolia-rpc.giwa.io",
       accounts: [PRIVATE_KEY],
-    },
-  },
-  chainDescriptors: {
-    91342: {
-      name: "Giwa Sepolia",
-      blockExplorers: {
-        blockscout: {
-          name: "Giwa Sepolia Explorer",
-          url: "https://sepolia-explorer.giwa.io",
-          apiUrl: "https://sepolia-explorer.giwa.io/api",
-        },
-      },
     },
   },
 };
